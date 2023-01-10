cac960c27a192378965985f596b0d171c1064d3a
Tedrosgh <tedrosgh@yahoo.com>
2023-01-10 05:59:29 -0600

    3 pages are deleted

--- src/App.js
+++ src/App.js
@@ -23,10 +23,10 @@ const App = () => {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/auth" exact component={Auth} />
-          <Route path="/mezmur" exact component={AllMezmurs} />
+          {/* <Route path="/mezmur" exact component={AllMezmurs} />
           <Route path="/mezmur/addmezmur" exact component={Add_new_mezmur} />
           <Route path="/mezmur/list" exact component={MezmurList}></Route>
-          <Route path="/mezmur/:id" exact component={SingleMezmur}></Route>
+          <Route path="/mezmur/:id" exact component={SingleMezmur}></Route> */}
         </Switch>
       </Container>
     </BrowserRouter>
